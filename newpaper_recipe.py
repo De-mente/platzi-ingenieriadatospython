35bbf4f36ce4d39a083c7aef6d6edf79e2c1fa37
De-mente <byron.campo@gmail.com>
2020-05-26 21:48:27 +0000

    A newspaper_recipe.py se ha agregado el llenado de títulos faltantes

--- newpaper_recipe.py
+++ newpaper_recipe.py
@@ -31,12 +31,25 @@ def _extract_host(df):
     return df
 
 
+def _fill_missings_titles(df):
+    logger.info('Filling missin titles')
+    missing_titles_mask = df['title'].isna()
+    missing_titles = (df[missing_titles_mask]['url']
+                    .str.extract(r'(?P<missing_titles>[^/]+)$')
+                    .applymap(lambda title: title.split('-'))
+                    .applymap(lambda title_word_list: ' '.join(title_word_list))
+                    )
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
+    return df
+
+
 def main(filename):
     logger.info('Starting cleaning process')
     df = _read_data(filename)
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missings_titles(df)
     return df
 
 
